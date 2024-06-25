433a21e7fba8ff5c9528df71f4bc2a0a4602a4f0
marek.bojdys <marek.bojdys@allegrogroup.com>
2024-06-25 23:39:34 +0200

    first commit

---
+++ src/test/java/CountLettersTest.java
@@ -0,0 +1,23 @@
+import org.example.Main;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
+
+public class CountLettersTest {
+
+    @ParameterizedTest
+    @CsvSource({
+            "Emilia,4",
+            "Jarosław,7",
+            "Maja,3",
+            "Stanisława,5"
+    })
+    public void isCompleted(String input, int expected) {
+        // when
+        int wynik = Main.countLettersAndReturnPosition(input);
+
+        // then
+        Assertions.assertEquals(expected, wynik);
+    }
+
+}
